4f2a7e403b17145f220ea3ca8034eada04fc7e56
Nikita <nik.sukovatov@mail.ru>
2023-10-18 12:49:54 +0300

    first commit

---
+++ src/js/modal.js
@@ -0,0 +1,27 @@
+
+var modal = document.getElementById("modal");
+var modalImage = modal.querySelector(".modal-image");
+var modalAvatar = modal.querySelector(".modal-avatar");
+var modalAuthorName = modal.querySelector(".modal-author-name");
+
+var images = document.querySelectorAll(".gelery .img");
+
+images.forEach(function(image) {
+  image.addEventListener("click", function() {
+    var bg = image.style.getPropertyValue("bg");
+    var avatar = image.querySelector(".avatar").src;
+    var authorName = image.querySelector(".author-name").textContent;
+  
+    modalImage.src = bg;
+    modalAvatar.src = avatar;
+    modalAuthorName.textContent = authorName;
+    
+    modal.style.display = "block";
+  });
+});
+
+var closeButton = modal.querySelector(".close");
+
+closeButton.addEventListener("click", function() {
+  modal.style.display = "none";
+});
